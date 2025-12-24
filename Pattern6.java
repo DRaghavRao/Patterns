fae2695f657d5eb5949419c9566a5dad8958e701
DRaghavRao <raghav2716@gmail.com>
2025-12-25 01:36:43 +0530

    Added all the patterns problem

---
+++ Pattern6.java
@@ -0,0 +1,12 @@
+public class Pattern6 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int k=1;k<=i;k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ Pattern8.java
@@ -0,0 +1,14 @@
+public class Pattern8 {
+    public static void main(String[] args) {
+        int n=5;
+        int number =1;
+        for(int i=1;i<=n;i++){
+            for(int k=1;k<=i;k++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+    
+}
